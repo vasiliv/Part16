cbca77468af7f77166988b690683368381af9d3b
vasiliv <ggo24@yahoo.com>
2019-08-12 10:47:04 +0400

    Add project files.

---
+++ Part16/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Part16.Controllers {
+    public class HomeController : Controller {
+        public ActionResult Index() {
+            TempData["myKey"] = "Data from index method";
+            return View();
+        }
+
+        public ActionResult Index2() {
+            //ViewBag.MyKey = TempData["myKey"];
+            ViewBag.MyKey = TempData.Peek("myKey");
+            TempData.Keep();
+            return View();
+        }
+
+        public ActionResult Index3() {
+            ViewBag.MyKey = TempData["myKey"];
+            return View();
+        }
+    }
+}
---
+++ Part16/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Part16.MvcApplication" Language="C#" %>
